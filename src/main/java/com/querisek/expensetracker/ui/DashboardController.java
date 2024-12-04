5b83b3119fd1809274b3ce1c5071e49acdd14820
Querisek <mrdasseran@gmail.com>
2024-12-04 12:26:53 +0100

    add date picker to dashboard

--- src/main/java/com/querisek/expensetracker/ui/DashboardController.java
+++ src/main/java/com/querisek/expensetracker/ui/DashboardController.java
@@ -4,6 +4,7 @@ import com.querisek.expensetracker.domain.AddExpenseRequest;
 import com.querisek.expensetracker.domain.Expense;
 import com.querisek.expensetracker.domain.ExpenseCreatedEvent;
 import com.querisek.expensetracker.domain.ExpenseRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Controller
 public class DashboardController {
@@ -21,13 +29,27 @@ public class DashboardController {
     }
 
     @GetMapping("/")
-    public String showDashboardToUser(@AuthenticationPrincipal UserDetails userDetails, Model model) {
-        model.addAttribute("userEmail", userDetails.getUsername());
-        model.addAttribute("addExpenseRequest", new AddExpenseRequest());
-        model.addAttribute("allUsersExpenses", expenseRepository.listUsersExpenses(userDetails.getUsername()));
-        double totalExpenses = expenseRepository.listUsersExpenses(userDetails.getUsername()).stream()
+    public String showDashboardToUser(@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
+            @AuthenticationPrincipal UserDetails userDetails,
+            Model model) {
+        LocalDate selectedDate = Objects.requireNonNullElseGet(date, LocalDate::now);
+        LocalDateTime startOfTheDay = selectedDate.atStartOfDay();
+        LocalDateTime endOfTheDay = selectedDate.atTime(23, 59, 59);
+        List<ExpenseCreatedEvent> filteredExpensesByDate = expenseRepository.listUsersExpenses(userDetails.getUsername()).stream()
+                .filter(expense -> {
+                    LocalDateTime expenseDate = expense.getExpenseCreatedAt();
+                    return !expenseDate.isBefore(startOfTheDay) && !expenseDate.isAfter(endOfTheDay);
+                })
+                .collect(Collectors.toList());
+
+        double totalExpenses = filteredExpensesByDate.stream()
                 .mapToDouble(ExpenseCreatedEvent::getPrice)
                 .sum();
+
+        model.addAttribute("selectedDate", selectedDate);
+        model.addAttribute("userEmail", userDetails.getUsername());
+        model.addAttribute("addExpenseRequest", new AddExpenseRequest());
+        model.addAttribute("allUsersExpenses", filteredExpensesByDate);
         model.addAttribute("totalUsersExpenses", totalExpenses);
         return "dashboard";
     }
