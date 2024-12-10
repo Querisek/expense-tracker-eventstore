51f5b9673be90706a65e089eed91d3feca7bf3b0
Querisek <mrdasseran@gmail.com>
2024-12-10 17:11:54 +0100

    categories for expenses page and icon change

--- src/main/java/com/querisek/expensetracker/ui/ExpensesController.java
+++ src/main/java/com/querisek/expensetracker/ui/ExpensesController.java
@@ -1,10 +1,16 @@
 package com.querisek.expensetracker.ui;
 
+import com.querisek.expensetracker.domain.expense.ExpenseCreatedEvent;
 import com.querisek.expensetracker.domain.expense.ExpenseRepository;
 import com.querisek.expensetracker.domain.income.IncomeRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/expenses")
@@ -16,7 +22,30 @@ public class ExpensesController {
     }
 
     @GetMapping
-    public String showAllExpensesToUser() {
+    public String showExpenseCategoriesToUser(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        List<ExpenseCreatedEvent> allExpenses = expenseRepository.listUsersExpensesByCategory(userDetails.getUsername(), "allCategories");
+
+        double totalExpenses = allExpenses.stream()
+                .mapToDouble(ExpenseCreatedEvent::getPrice)
+                .sum();
+
+        double totalFood = expenseRepository.getTotalPriceOfCategoryByDay("Jedzenie", allExpenses);
+        double totalTravels = expenseRepository.getTotalPriceOfCategoryByDay("Podróże", allExpenses);
+        double totalHealth = expenseRepository.getTotalPriceOfCategoryByDay("Zdrowie", allExpenses);
+        double totalEntertainment = expenseRepository.getTotalPriceOfCategoryByDay("Rozrywka", allExpenses);
+        double totalHome = expenseRepository.getTotalPriceOfCategoryByDay("Dom", allExpenses);
+        double totalOthers = expenseRepository.getTotalPriceOfCategoryByDay("Inne", allExpenses);
+
+        model.addAttribute("userEmail", userDetails.getUsername());
+        model.addAttribute("allExpenses", allExpenses);
+        model.addAttribute("totalExpenses", totalExpenses);
+        model.addAttribute("totalFood", totalFood);
+        model.addAttribute("totalTravels", totalTravels);
+        model.addAttribute("totalHealth", totalHealth);
+        model.addAttribute("totalEntertainment", totalEntertainment);
+        model.addAttribute("totalHome", totalHome);
+        model.addAttribute("totalOthers", totalOthers);
+
         return "expenses";
     }
 }
