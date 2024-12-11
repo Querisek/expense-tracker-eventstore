6ee481550f488c74d89d9223789e82dcf108dbad
Querisek <mrdasseran@gmail.com>
2024-12-11 21:05:43 +0100

    fix redirecting

--- src/main/java/com/querisek/expensetracker/ui/ExpensesController.java
+++ src/main/java/com/querisek/expensetracker/ui/ExpensesController.java
@@ -1,16 +1,19 @@
 package com.querisek.expensetracker.ui;
 
+import com.querisek.expensetracker.domain.expense.AddExpenseRequest;
+import com.querisek.expensetracker.domain.expense.Expense;
 import com.querisek.expensetracker.domain.expense.ExpenseCreatedEvent;
 import com.querisek.expensetracker.domain.expense.ExpenseRepository;
 import com.querisek.expensetracker.domain.income.IncomeRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/expenses")
@@ -47,5 +50,43 @@ public class ExpensesController {
         model.addAttribute("totalOthers", totalOthers);
 
         return "expenses";
+    }
+
+    @PostMapping
+    public String addExpense(@ModelAttribute AddExpenseRequest expenseRequest, @AuthenticationPrincipal UserDetails userDetails, HttpServletRequest httpRequest) {
+        Expense expense = new Expense(
+                userDetails.getUsername(),
+                expenseRequest.getExpenseCategory(),
+                expenseRequest.getExpenseDescription(),
+                expenseRequest.getPrice()
+        );
+        expenseRepository.addExpense(expense);
+        if(httpRequest.getHeader("Referer") != null) {
+            return "redirect:" + httpRequest.getHeader("Referer");
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/delete/{expenseId}")
+    public String deleteExpense(@PathVariable UUID expenseId, @AuthenticationPrincipal UserDetails userDetails, HttpServletRequest httpRequest) {
+        List<ExpenseCreatedEvent> allExpenses = expenseRepository.listUsersExpensesByCategory(userDetails.getUsername(), "allCategories");
+        ExpenseCreatedEvent expenseToDelete = allExpenses.stream()
+                .filter(e -> e.getExpenseId().equals(expenseId))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Wybrany wydatek nie istnieje"));
+        Expense expense = new Expense(
+                expenseToDelete.getExpenseId(),
+                expenseToDelete.getUserId(),
+                expenseToDelete.getExpenseCategory(),
+                expenseToDelete.getExpenseDescription(),
+                expenseToDelete.getPrice()
+        );
+        expenseRepository.deleteExpense(expense);
+        if(httpRequest.getHeader("Referer") != null) {
+            return "redirect:" + httpRequest.getHeader("Referer");
+        } else {
+            return "redirect:/";
+        }
     }
 }
