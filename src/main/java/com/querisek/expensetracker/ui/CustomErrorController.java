d0bf531201ab469dd895b34b98d2c5a54e1ea3d5
Querisek <mrdasseran@gmail.com>
2024-12-11 21:38:47 +0100

    add controller for custom error handling

---
+++ src/main/java/com/querisek/expensetracker/ui/CustomErrorController.java
@@ -0,0 +1,14 @@
+package com.querisek.expensetracker.ui;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String redirectUserToHomepageAfterError() {
+        return "redirect:/";
+    }
+}
